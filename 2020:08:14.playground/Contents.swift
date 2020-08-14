0fa414bd375ed6c9caa48b20a8d24565bdb50ae4
yosshi4486 <yoshiharuseijin@gmail.com>
2020-08-14 21:45:13 +0900

    21:45

--- 2020:08:14.playground/Contents.swift
+++ 2020:08:14.playground/Contents.swift
@@ -25,3 +25,44 @@ print(string)
  ・PlaygroundのInspectorPaneから「Render Documentation」のチェックボックスを入れると、マークダウンをレンダリングしてくれます。
  */
 let sampleImage = UIImage(named: "Rendered")
+
+/*:
+ ・SwiftUIは、静的なView作成とSource of Truthのツールセットは素晴らしいけど、動的なViewは弱いし、基本的にはシンタックス不足という事で、
+ SwiftUI2の現状ではUIKit/AppKitと半々ぐらいの利用率を目指していきたい。
+ 
+ ・[UICollectionViewDiffableDataSource](https://developer.apple.com/documentation/uikit/uicollectionviewdiffabledatasource)は、UICollectionViewDataSourceプロトコルに適合している。これまでDataSourceに対する大量のボイラープレートを実装していたのが、公式にサポートされる感じみたい。あとは、ネットワークから取得してきたデータの反映とかもややこしかったので、差分管理の仕組みが入ったぽい。
+ 
+ ・あーはいはい。今までFlowLayout渡していたところでListConfiguration含んだLayoutが渡せるようになったのか。
+ */
+
+//: ### Before
+let collectionViewFlowLayout = UICollectionViewFlowLayout()
+collectionViewFlowLayout.itemSize = CGSize(width: 50, height: 50)
+collectionViewFlowLayout.minimumLineSpacing = 5
+collectionViewFlowLayout.minimumLineSpacing = 5
+collectionViewFlowLayout.sectionInset = .init(top: 5, left: 5, bottom: 5, right: 5)
+
+let floyLayoutCollectionView = UICollectionView(frame: .init(x: 0, y: 0, width: 300, height: 500), collectionViewLayout: collectionViewFlowLayout)
+
+//: ### Now
+var listConfiguration = UICollectionLayoutListConfiguration(appearance: .sidebar)
+let compositionalLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+let listAppearanceCollectionView = UICollectionView(frame: .init(x: 0, y: 0, width: 300, height: 500), collectionViewLayout: compositionalLayout)
+
+//: ここでUICollectionViewCompositionalLayoutはUICollectionViewLayoutのサブクラス。つまり提供されたAPIの内部では渡されたConfigurationを使ってよしなにListを構築してくれるような実装がなされているわけだ。
+
+//: スワイプのAPIの場所がWWDCの時と変わっているみたい
+//: このように利用できる。
+
+listConfiguration.leadingSwipeActionsConfigurationProvider = { indexPath in
+    
+    let indent = UIContextualAction(style: .normal, title: "Indent") { (_, _, completion) in
+        completion(true)
+    }
+    
+    return UISwipeActionsConfiguration(actions: [indent])
+}
+
+/*:
+ ・Things3は流石に上手いなぁ...。称賛を送るしかない。セルタップで、まさかセルのエリアが拡大してiPhoneでフォーカスを表現するとは思わんかった。なるほど確かに、それなら一覧のコンテキストを保持したまま、テンポラリモードだけで入力・編集を終えられる。これは本当にすごい。発明。
+ */
