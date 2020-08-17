0da275154b6c7e7f4bb2a5df3d4da22a0b9d5580
yosshi4486 <yoshiharuseijin@gmail.com>
2020-08-17 15:22:45 +0900

    15:22

---
+++ 2020:08:17.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+/*:
+ ・for-inループと、forEachの違いについてはっきりこれまで説明できなかったけど、公式ドキュメントに書いてあった。
+ 
+ https://developer.apple.com/documentation/swift/array/1689783-foreach
+ 
+ ・for-inループはreturnやbreakなど、エスケープステートメント(?正式名称が分からない)がうまく使えるので、ループなどを活用して条件に適応したら抜けるような場合は、結構難しい。
+ */
