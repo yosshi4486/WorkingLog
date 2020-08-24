2a2c12e63fbebce94267f127339b748231557595
yosshi4486 <yoshiharuseijin@gmail.com>
2020-08-24 13:56:22 +0900

    13:56

---
+++ 8-23~30.playground/Pages/8-24.xcplaygroundpage/Sources/Benchmark.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+// https://qiita.com/tady/items/40d7c4feecda337cf271から拝借
+public struct Benchmark {
+    
+    // 開始時刻を保存する変数
+    var startTime: Date
+    var key: String
+    
+    // 処理開始
+    init(key: String) {
+        self.startTime = Date()
+        self.key = key
+    }
+    
+    // 処理終了
+    func finish() {
+        let elapsed = Date().timeIntervalSince(startTime) as Double
+        let formatedElapsed = String(format: "%.3f", elapsed)
+        print("Benchmark: \(key), Elasped time: \(formatedElapsed)(s)")
+    }
+    
+    // 処理をブロックで受け取る
+    public static func measure(key: String, block: () -> ()) {
+        let benchmark = Benchmark(key: key)
+        block()
+        benchmark.finish()
+    }
+    
+}
